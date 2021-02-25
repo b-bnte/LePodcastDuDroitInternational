848a2866bbecab5e0ff8d42af0c34aa3ce089464
b-bnte <bastien.bonte@outlook.fr>
2021-02-25 16:17:18 +0100

    add hide and show descriptions of podcasts

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-/* inutile pour le moment */
+/* Hello World */
+
+/*alert('Hello world');
+ */
+
+function hello(i) {
+    alert('Hello world');
+}
+
+function showDescriptionPodcast(id)
+{
+    if (document.getElementById(id).classList == 'podcastDescription_hide') {
+        document.getElementById(id).classList.add('podcastDescription_show');
+        document.getElementById(id).classList.remove('podcastDescription_hide');
+    }
+    else {
+        document.getElementById(id).classList.add('podcastDescription_hide');
+        document.getElementById(id).classList.remove('podcastDescription_show');
+    }
+    
+}
