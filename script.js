2aec10703b3b1a692b3fe9be874d9e117f0046d8
b-bnte <bastien.bonte@outlook.fr>
2021-11-25 23:03:12 +0100

    ad episode 10, Abdoulaye Sow.

--- script.js
+++ script.js
@@ -17,5 +17,36 @@ function showDescriptionPodcast(id)
         document.getElementById(id).classList.add('podcastDescription_hide');
         document.getElementById(id).classList.remove('podcastDescription_show');
     }
-    
+}
+
+function showSeason(button_id, id) {
+
+    // Button
+    document.getElementById(button_id).classList.add('raise_pressed')
+    document.getElementById(button_id).classList.remove('raise')
+
+    if (button_id == "btn_season1") {
+        document.getElementById("btn_season1").classList.add('raise_pressed')
+        document.getElementById("btn_season1").classList.remove('raise')
+        document.getElementById("btn_season2").classList.add('raise')
+        document.getElementById("btn_season2").classList.remove('raise_pressed')
+    }
+    else {
+        document.getElementById("btn_season2").classList.add('raise_pressed')
+        document.getElementById("btn_season2").classList.remove('raise')
+        document.getElementById("btn_season1").classList.add('raise')
+        document.getElementById("btn_season1").classList.remove('raise_pressed')
+    }
+
+    // Episodes
+    var x = document.getElementById(id)
+
+    document.getElementById("season1").style.display = "none"
+    document.getElementById("season2").style.display = "none"
+
+    if (x.style.display === "none") {
+        x.style.display = "block"
+    } else {
+        x.style.display = "none"
+    }
 }
