55d77cc78aed0388baa284ff18620d903e0cee6c
Bastien BONTE <bastien.bonte@overkiz.com>
2023-06-05 23:52:14 +0200

    add THT episode.

--- script.js
+++ script.js
@@ -22,27 +22,49 @@ function showDescriptionPodcast(id)
 function showSeason(button_id, id) {
 
     // Button
-    document.getElementById(button_id).classList.add('raise_pressed')
-    document.getElementById(button_id).classList.remove('raise')
+    if (button_id == "btn_tht") {
+      document.getElementById(button_id).classList.add('raise_pressed_red')
+      document.getElementById(button_id).classList.remove('raise_red')
+    }
+    else {
+      document.getElementById(button_id).classList.add('raise_pressed')
+      document.getElementById(button_id).classList.remove('raise')
+    }
 
     if (button_id == "btn_season1") {
         document.getElementById("btn_season1").classList.add('raise_pressed')
         document.getElementById("btn_season1").classList.remove('raise')
+
+        document.getElementById("btn_season2").classList.add('raise')
+        document.getElementById("btn_season2").classList.remove('raise_pressed')  
+        document.getElementById("btn_tht").classList.add('raise_red')
+        document.getElementById("btn_tht").classList.remove('raise_pressed_red')
+    }
+    else if (button_id == "btn_tht") {
+        document.getElementById("btn_tht").classList.add('raise_pressed_red')
+        document.getElementById("btn_tht").classList.remove('raise_red')
+
         document.getElementById("btn_season2").classList.add('raise')
         document.getElementById("btn_season2").classList.remove('raise_pressed')
-    }
-    else {
-        document.getElementById("btn_season2").classList.add('raise_pressed')
-        document.getElementById("btn_season2").classList.remove('raise')
         document.getElementById("btn_season1").classList.add('raise')
         document.getElementById("btn_season1").classList.remove('raise_pressed')
     }
+    else {
+      document.getElementById("btn_season2").classList.add('raise_pressed')
+      document.getElementById("btn_season2").classList.remove('raise')
+
+      document.getElementById("btn_season1").classList.add('raise')
+      document.getElementById("btn_season1").classList.remove('raise_pressed')
+      document.getElementById("btn_tht").classList.add('raise_red')
+      document.getElementById("btn_tht").classList.remove('raise_pressed_red')
+  }
 
     // Episodes
     var x = document.getElementById(id)
 
     document.getElementById("season1").style.display = "none"
     document.getElementById("season2").style.display = "none"
+    document.getElementById("tht").style.display = "none"
 
     if (x.style.display === "none") {
         x.style.display = "block"
